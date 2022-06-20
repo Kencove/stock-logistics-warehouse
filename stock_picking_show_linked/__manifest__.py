80d74267756264de1ded29402daccaa92321db25
Juany Davila <juanyadavilap@gmail.com>
2022-06-20 10:29:56 -0500

    [ADD] stock_picking_show_linked: Add to 14.0

---
+++ stock_picking_show_linked/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018-20 ForgeFlow S.L. (https://www.forgeflow.com)
+# Part of ForgeFlow. See LICENSE file for full copyright and licensing details.
+{
+    "name": "Stock Picking Show Linked",
+    "summary": "DDMRP Alerts",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": [
+        "stock",
+    ],
+    "data": ["views/stock_picking.xml"],
+    "license": "AGPL-3",
+    "installable": True,
+}
